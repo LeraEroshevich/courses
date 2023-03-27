9574c6d0b50165058c5dcfdd7345db23c25abe65
LeraEroshevich <leraeroshevich99@gmail.com>
2023-03-27 21:35:30 +0300

    new

--- lesson1/src/main/java/Variant_3.java
+++ lesson1/src/main/java/Variant_3.java
@@ -1,7 +1,5 @@
-package main.java;
-
 public class Variant_3 {
     public static void main(String[] args) {
-        System.out.println("Hello, Variant_3!");
+        System.out.println("Hello world!");
     }
 }
