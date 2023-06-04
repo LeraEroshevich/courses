c1b5b9636ed90af1fe891d434ea6e746a6fa3530
LeraEroshevich <leraeroshevich99@gmail.com>
2023-06-04 22:07:24 +0300

    fixed

--- lesson15/src/test/java/TasksTest.java
+++ lesson15/src/test/java/TasksTest.java
@@ -1,4 +1,3 @@
-import java.util.List;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -37,17 +36,19 @@ public class TasksTest {
     By subMenuDocsLocator = By.xpath("//div[@id='docs']//a[@href='https://github.com/rest-assured/rest-assured/wiki/GettingStarted']");
     By searchTitleLocator = By.xpath("//div [@id='wiki-wrapper']//h1");
     By subMenuDownloadsLocator = By.xpath("//div[@id='docs']//a[@href='https://github.com/rest-assured/rest-assured/wiki/Downloads']");
-    By searchTitleTwoLocator = By.xpath("//div[@id='wiki-body']//a[@id='user-content-downloads']");
+    By searchTitleTwoLocator = By.xpath("//div[@id='wiki-body']//div[@class='markdown-body']//h1");
+
     @Test
-    void task1Test(){
+    void ImgInPngTest(){
         driver.get("https://www.jenkins.io/");
         driver.manage().window().maximize();
         WebElement gridComponentsImg = driver.findElement(gridComponentsImgLocator);
         String imgLink = gridComponentsImg.getAttribute("src");
         Assertions.assertTrue(imgLink.endsWith(".png"));
     }
+
     @Test
-    void task2Test(){
+    void CheckTextOutputAndCorrectLinkTest(){
         driver.get("https://rest-assured.io/");
         driver.manage().window().maximize();
         WebElement menuItemGrid = driver.findElement(By.xpath("//div[@id='main']//li[@class='p-docs']"));
@@ -57,19 +58,17 @@ public class TasksTest {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         jsExecutor.executeScript("arguments[0].click();",subMenuGrid);
 
-        WebElement searchTitle = driver.findElement(searchTitleLocator);
-        if (searchTitle.isDisplayed()){
-            System.out.println("The text 'GettingStarted' is visible");
-        } else {
-            System.out.println("'GettingStarted' text not visible");
-        }
+        String searchTitle = driver.findElement(searchTitleLocator).getText();
+        boolean TextDisplayed = driver.findElement(searchTitleLocator).isDisplayed();
+        Assertions.assertEquals("GettingStarted", searchTitle);
+        Assertions.assertTrue(TextDisplayed);
 
         String currentUrl = driver.getCurrentUrl();
         Assertions.assertEquals("https://github.com/rest-assured/rest-assured/wiki/GettingStarted", currentUrl);
     }
 
     @Test
-    void task3Test() {
+    void CheckColumnsAndRowsInTableTest() {
         driver.get("https://rest-assured.io/");
         driver.manage().window().maximize();
         WebElement menuItemGrid = driver.findElement(By.xpath("//div[@id='main']//li[@class='p-docs']"));
@@ -79,28 +78,18 @@ public class TasksTest {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         jsExecutor.executeScript("arguments[0].click();", subMenuDownloads);
 
-        WebElement searchTitle = driver.findElement(searchTitleTwoLocator);
-        if (searchTitle.isDisplayed()) {
-            System.out.println("The text 'Downloads' is visible");
-        } else {
-            System.out.println("'Downloads' text not visible");
-        }
+        String searchTitle = driver.findElement(searchTitleTwoLocator).getText();
+        boolean TextDisplayed = driver.findElement(searchTitleTwoLocator).isDisplayed();
+        Assertions.assertEquals("Downloads", searchTitle);
+        Assertions.assertTrue(TextDisplayed);
 
         String currentUrl = driver.getCurrentUrl();
         Assertions.assertEquals("https://github.com/rest-assured/rest-assured/wiki/Downloads", currentUrl);
 
         WebElement attributesTable = driver.findElement(By.xpath("//table[@role='table']"));
-        List<WebElement> rows = attributesTable.findElements(By.tagName("tr"));
-
-        int rowCount = rows.size() - 1;
-
-        List<WebElement> columns = rows.get(1).findElements(By.tagName("td"));
-        int columnCount = columns.size();
-
-        if (rowCount == 8 && columnCount == 2) {
-            System.out.println("Attribute table contains 2 columns and 8 rows");
-        } else {
-            System.out.println("Attribute table does not match expected values");
-        }
+        int tableRows = attributesTable.findElements(By.xpath("//table[@role='table']//thead//tr//th")).size();
+        Assertions.assertEquals(2, tableRows);
+        int tableColumns = attributesTable.findElements(By.xpath("//table[@role='table']//tbody//tr")).size();
+        Assertions.assertEquals(8, tableColumns);
     }
 }
