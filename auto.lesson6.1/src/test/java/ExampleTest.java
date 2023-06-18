5ee94a70eb3a4fef7f9409d44b6ca63636497066
LeraEroshevich <leraeroshevich99@gmail.com>
2023-06-18 17:40:30 +0300

    auto.lesson6.1

---
+++ auto.lesson6.1/src/test/java/ExampleTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class ExampleTest {
+
+    public static Stream<String[]> testData() {
+        return Stream.of(
+            new String[]{"testing", "tested"},
+            new String[]{"testeddddd", "testing"}
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    public void testStringLength(String firstValue, String secondValue) {
+        Assertions.assertTrue(firstValue.length() > secondValue.length(),
+                              "Длина первого значения набора должна быть больше длины второго значения");
+
+    }
+}
