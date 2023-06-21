af37760022e4d7b780ae59833803d851c6187ff4
LeraEroshevich <leraeroshevich99@gmail.com>
2023-06-22 00:37:50 +0300

    fixed task1

--- lesson16/src/test/java/TasksTest.java
+++ lesson16/src/test/java/TasksTest.java
@@ -32,6 +32,9 @@ public class TasksTest {
     void startBrowser(){
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+        // Инициализация WebDriver
+        System.setProperty("webdriver.chrome.driver", "путь_к_драйверу_chrome");
+        driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -42,7 +45,7 @@ public class TasksTest {
     By mainMenuItemLocator = By.xpath("//nav[@data-test='main-menu']//div[@data-test-marker='Developer Tools']");
     By mainSubMenuLocator = By.xpath("//nav[@data-test='main-menu']//div[@data-test='main-submenu']//a[@href='https://plugins.jetbrains.com/search?tags=Theme']");
     By inputFieldLocator = By.xpath("//div[@class='search-box']//div[@data-testid='autocomplete']//input[@type='text']");
-    By autocompleteElementLocator = By.xpath("//div[contains(@Class, 'autocomplete__item')]//div[@Class='autocomplete__element']");
+    //By autocompleteElementLocator = By.xpath("//div[contains(@Class, 'autocomplete__item')]//div[@Class='autocomplete__element']");
     By ratingStarsLocator = By.xpath("//span[@class='test_plugin-header-rate']//span[@data-test='rate']");
     By ratingStarLocator = By.xpath("//span[@class='test_plugin-header-rate']//span[@data-test='rate']//span[@data-test='rate-star']");
     By mainSubCodeMenuLocator = By.xpath("//nav[@data-test='main-menu']//div[@data-test='main-submenu']//a[@href='/code-with-me/']");
@@ -51,9 +54,33 @@ public class TasksTest {
     By iFrameLocator = By.xpath("//iframe");
     By mainSubScalaMenuLocator = By.xpath("//nav[@data-test='main-menu']//div[@data-test='main-submenu']//span[text()='Scala']");
     By gridComponentsImgLocator = By.xpath("//img[@alt='Plugin Logo']");
+     By autocompleteItemLocator = By.xpath("//div[contains(@class, 'autocomplete__item')]");
+     By autocompleteElementLocator = By.xpath(".//div[contains(@class, 'autocomplete__element')]");
+
+    public TasksTest(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement findAutocompleteElementByText(String findText) {
+        List<WebElement> autocompleteItems = new WebDriverWait(driver, Duration.ofSeconds(5))
+            .until(ExpectedConditions.presenceOfAllElementsLocatedBy(autocompleteItemLocator));
+
+        for (WebElement item : autocompleteItems) {
+            WebElement element = item.findElement(By.xpath(".//div[contains(@class, 'autocomplete__element')]"));
+            String itemText = element.getText();
+
+            itemText = itemText.toLowerCase().replaceAll("\\s+", "");
+            findText = findText.toLowerCase().replaceAll("\\s+", "");
+
+            if (itemText.contains(findText)) {
+                return element;
+            }
+        }
+        return null;
+    }
 
     @Test
-    void checkRatingVisibility(){
+    void checkRatingVisibility() {
         driver.get(URL_JETBRAINS);
         WebElement mainMenuItem = driver.findElement(mainMenuItemLocator);
         mainMenuItem.click();
@@ -64,12 +91,12 @@ public class TasksTest {
         WebElement inputField = driver.findElement(inputFieldLocator);
         inputField.sendKeys("Dracula Theme");
 
-       WebElement autocompleteElement = new WebDriverWait(driver, Duration.ofSeconds(30))
-                   .until(ExpectedConditions.elementToBeClickable(autocompleteElementLocator));
-       autocompleteElement.click();
+        WebElement autocompleteElement = new WebDriverWait(driver, Duration.ofSeconds(30))
+            .until(ExpectedConditions.elementToBeClickable(autocompleteElementLocator));
+        autocompleteElement.click();
 
         WebElement ratingStars = new WebDriverWait(driver, Duration.ofSeconds(30))
-                    .until(ExpectedConditions.elementToBeClickable(ratingStarsLocator));
+            .until(ExpectedConditions.elementToBeClickable(ratingStarsLocator));
         boolean isDisplayed = ratingStars.isDisplayed();
         Assertions.assertTrue(isDisplayed, "Star rating not visible");
 
